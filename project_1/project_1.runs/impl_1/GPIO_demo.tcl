5f4e1fef12eaeb602e41d5b0e39982157715dd5d
esantosrcj <esantosrcj@gmail.com>
2015-09-30 12:54:15 -0500

    Introduction to Xilinx Design Suite and Nexys Artix-7 FPGA board

---
+++ project_1/project_1.runs/impl_1/GPIO_demo.tcl
@@ -0,0 +1,65 @@
+proc start_step { step } {
+  set stopFile ".stop.rst"
+  if {[file isfile .stop.rst]} {
+    puts ""
+    puts "*** Halting run - EA reset detected ***"
+    puts ""
+    puts ""
+    return -code error
+  }
+  set beginFile ".$step.begin.rst"
+  set platform "$::tcl_platform(platform)"
+  set user "$::tcl_platform(user)"
+  set pid [pid]
+  set host ""
+  if { [string equal $platform unix] } {
+    if { [info exist ::env(HOSTNAME)] } {
+      set host $::env(HOSTNAME)
+    }
+  } else {
+    if { [info exist ::env(COMPUTERNAME)] } {
+      set host $::env(COMPUTERNAME)
+    }
+  }
+  set ch [open $beginFile w]
+  puts $ch "<?xml version=\"1.0\"?>"
+  puts $ch "<ProcessHandle Version=\"1\" Minor=\"0\">"
+  puts $ch "    <Process Command=\".planAhead.\" Owner=\"$user\" Host=\"$host\" Pid=\"$pid\">"
+  puts $ch "    </Process>"
+  puts $ch "</ProcessHandle>"
+  close $ch
+}
+
+proc end_step { step } {
+  set endFile ".$step.end.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+proc step_failed { step } {
+  set endFile ".$step.error.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+set_msg_config -id {HDL 9-1061} -limit 100000
+set_msg_config -id {HDL 9-1654} -limit 100000
+
+start_step write_bitstream
+set rc [catch {
+  create_msg_db write_bitstream.pb
+  set_param gui.test TreeTableDev
+  open_checkpoint GPIO_demo_routed.dcp
+  write_bitstream -force GPIO_demo.bit 
+  if { [file exists H:/Spring2015/EECS443/project_1/project_1.runs/synth_1/GPIO_demo.hwdef] } {
+    catch { write_sysdef -hwdef H:/Spring2015/EECS443/project_1/project_1.runs/synth_1/GPIO_demo.hwdef -bitfile GPIO_demo.bit -meminfo GPIO_demo_bd.bmm -file GPIO_demo.sysdef }
+  }
+  close_msg_db -file write_bitstream.pb
+} RESULT]
+if {$rc} {
+  step_failed write_bitstream
+  return -code error $RESULT
+} else {
+  end_step write_bitstream
+}
+
